bc9e42b14722eeb0789cacc940cd0897944e2418
devuser <devuser@debian11-2.168.1.6>
2024-03-17 21:10:37 -0300

    add doxygen

--- lib/libdyn/include/dynamic.h
+++ lib/libdyn/include/dynamic.h
@@ -1,3 +1,14 @@
 #pragma once
 
+/**
+*   Funcion que calcula y obtiene la potencia de un numero.
+*   Se incluye de dinamicamente
+*
+*   @param
+*   - base: double
+*   - exp:  int
+*
+*   @return
+*   - result: int
+*/
 double mypow_dynamic(double x, int exp);
